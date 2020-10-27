a2974bd422ba42a44eafdaf364b3d3aaf422d93c
fanhl <fanhl@189.cn>
2020-10-27 15:37:54 +0800

    add 验证码

--- hooks/umi3/src/pages/user/login/index.tsx
+++ hooks/umi3/src/pages/user/login/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.less';
 
-import { Tabs, Form, Input, Button, Checkbox, Row, Col } from 'antd';
+import {
+  Tabs,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Row,
+  Col,
+  Statistic,
+  message,
+} from 'antd';
+
 import { Link, useModel, history, History } from 'umi';
 import {
   UserOutlined,
@@ -17,70 +28,128 @@ import {
 } from '@ant-design/icons';
 
 const { TabPane } = Tabs;
-
-function callback(key: any) {
-  console.log(key);
-}
+const { Countdown } = Statistic;
 
 export default () => {
+  const [loginType, setLoginType] = useState(1);
+  const [autoLogin, setAutoLogin] = useState(true);
+  const [captchaFlag, setCaptchaFlag] = useState(false);
+
+  const onSubmit = (values: any) => {
+    console.log('Received values of form: ', values);
+    console.log(autoLogin);
+  };
+
+  const onTabChange = (activeKey: any) => {
+    console.log(activeKey);
+    setLoginType(activeKey);
+  };
+
+  function onFinish() {
+    console.log('finished!');
+    setCaptchaFlag(false);
+  }
+
+  const onCaptchaClick = () => {
+    console.log('test');
+    message.success('获取验证码成功！验证码为：1234');
+    setCaptchaFlag(true);
+  };
+
+  const deadline = Date.now() + 1000 * 15; // Moment is also OK
+
   return (
     <div className={styles.main}>
-      <Form size="large">
-        <Tabs defaultActiveKey="2" onClick={callback} centered>
+      <Form size="large" onFinish={onSubmit}>
+        <Tabs defaultActiveKey="1" onChange={onTabChange} centered>
           <TabPane tab="账户密码登录" key="1">
-            <Form.Item
-              name="username"
-              rules={[{ required: true, message: '请输入用户名!' }]}
-            >
-              <Input
-                prefix={<UserOutlined className={styles.prefixIcon} />}
-                placeholder="用户名: admin or user"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[{ required: true, message: '请输入密码！' }]}
-            >
-              <Input
-                prefix={<LockTwoTone className={styles.prefixIcon} />}
-                placeholder="密码: ant.design"
-              />
-            </Form.Item>
+            {1 == loginType && (
+              <div>
+                <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: '请输入用户名!' }]}
+                >
+                  <Input
+                    prefix={<UserOutlined className={styles.prefixIcon} />}
+                    placeholder="用户名: admin or user"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: '请输入密码！' }]}
+                >
+                  <Input
+                    prefix={<LockTwoTone className={styles.prefixIcon} />}
+                    placeholder="密码: ant.design"
+                  />
+                </Form.Item>
+              </div>
+            )}
           </TabPane>
           <TabPane tab="手机号登录" key="2">
-            <Form.Item
-              name="mobile"
-              rules={[
-                { required: true, message: '请输入手机号!' },
-                { pattern: /^1\d{10}$/, message: '手机号格式错误！' },
-              ]}
-            >
-              <Input
-                prefix={<MobileTwoTone className={styles.prefixIcon} />}
-                placeholder="手机号"
-              />
-            </Form.Item>
-            <Form.Item
-              name="captcha"
-              rules={[{ required: true, message: '请输入验证码' }]}
-            >
-              <Row gutter={8} justify="end">
-                <Col span={16}>
+            {2 == loginType && (
+              <div>
+                <Form.Item
+                  name="mobile"
+                  rules={[
+                    { required: true, message: '请输入手机号!' },
+                    { pattern: /^1\d{10}$/, message: '手机号格式错误！' },
+                  ]}
+                >
                   <Input
-                    prefix={<MailTwoTone className={styles.prefixIcon} />}
-                    placeholder="验证码"
+                    prefix={<MobileTwoTone className={styles.prefixIcon} />}
+                    placeholder="手机号"
                   />
-                </Col>
-                <Col span={8} className={styles.captcha_button}>
-                  <Button>获取验证码</Button>
-                </Col>
-              </Row>
-            </Form.Item>
+                </Form.Item>
+                <Form.Item
+                  name="captcha"
+                  rules={[{ required: true, message: '请输入验证码' }]}
+                >
+                  <Row gutter={8} justify="end">
+                    <Col span={16}>
+                      <Input
+                        prefix={<MailTwoTone className={styles.prefixIcon} />}
+                        placeholder="验证码"
+                      />
+                    </Col>
+                    <Col span={8} className={styles.captcha_button}>
+                      <Button
+                        style={{ width: '100%' }}
+                        disabled={captchaFlag}
+                        onClick={onCaptchaClick}
+                      >
+                        {captchaFlag ? (
+                          <Countdown
+                            value={deadline}
+                            format="s 秒"
+                            valueStyle={{
+                              color: 'rgba(0, 0, 0, 0.25)',
+                              fontSize: 16,
+                            }}
+                            onFinish={onFinish}
+                          />
+                        ) : (
+                          '获取验证码'
+                        )}
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form.Item>
+              </div>
+            )}
           </TabPane>
         </Tabs>
 
         <div>
-          <Checkbox>自动登录</Checkbox>
+          <Checkbox
+            checked={autoLogin}
+            onChange={(e) => {
+              console.log(e.target.checked);
+              setAutoLogin(e.target.checked);
+            }}
+          >
+            自动登录
+          </Checkbox>
           <a
             style={{
               float: 'right',
