4dc95a9da26aacee73d5935e55599a17d6e50a6a
fanhl <fanhl@189.cn>
2021-02-17 10:23:48 +0800

    add new page

--- hooks/slate/src/index.js
+++ hooks/slate/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Slate from './pages/slate';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Slate />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ hooks/slate/src/pages/slate.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './slate.css';
+export default()=>{
+    return <div className="app">dddds</div>
+}
